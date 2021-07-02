a766368f45b1ccd5d1ae6eeb0aed1ebb62b86ec7
cathorse236 <s1280236@u-aizu.ac.jp>
2021-07-02 15:30:59 +0900

    Greetings added

--- exercise4.c
+++ exercise4.c
@@ -4,11 +4,14 @@
 
 int main() {
 	int a, b;
+	char name[30];
+	printf("What is your name?\n");
+	scanf("%s", name);
+	printf("Hello, %s!\n", name);
 	printf("Rolling the dice...\n");
 	srand((unsigned int) time(NULL));
 	a = rand() % 6 + 1;
 	printf("Die 1: %d\n", a);
-	srand((unsigned int) time(NULL)+3);
 	b = rand() % 6 + 1;
 	printf("Die 2: %d\n", b);
 	printf("Total value: %d\n", a+b);
