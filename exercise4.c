0d9123616dc0fac744d7209c2e34c37346cefeec
cathorse236 <s1280236@u-aizu.ac.jp>
2021-07-02 15:45:09 +0900

    Won/lost message added

--- exercise4.c
+++ exercise4.c
@@ -4,14 +4,15 @@
 
 int main() {
 	int a, b;
+	
 	printf("Rolling the dice...\n");
 	srand((unsigned int) time(NULL));
 	a = rand() % 6 + 1;
 	printf("Die 1: %d\n", a);
-	srand((unsigned int) time(NULL)+3);
 	b = rand() % 6 + 1;
 	printf("Die 2: %d\n", b);
 	printf("Total value: %d\n", a+b);
-
+	if (a + b > 7) printf("You won.\n");
+	else printf("You lost.\n");
 	return 0;
 }
